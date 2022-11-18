9dc1ae9ebf41583be94131614f8e360603ba11c9
serkan <serkan.satirx61@gmail.com>
2022-11-18 08:46:09 +0100

    finished 01 JS Variablen und Operationen

---
+++ 01_Variablen_und_Operationen/script.js
@@ -0,0 +1,58 @@
+console.log("Hallo");
+
+// Variablen
+let a = 7; // Wir setzen die Variable a und können sie an jedem Punkt so überschreiben wie es uns passt
+let b = 8;
+let c = 10;
+
+// Berechne folgendes und gib das Eregebnis über console.log aus 
+
+// a + b + c 
+
+let result = a + b + c;
+console.log("Das Ergebnis beträgt: " + result);
+
+// (a + b) / c 
+
+result = (a+b)/c
+console.log("Das Ergebnis beträgt: " + result);
+
+// c -a + b 
+
+result = c - a +b;
+console.log("Das Ergebnis beträgt: " + result);
+
+// a + b + c * 3 
+
+result = a + b + (c * 3);
+console.log("Das Ergebnis beträgt: " + result);
+
+// (a + b + c)/4 
+result = (a + b + c) / 4;
+console.log("Das Ergebnis beträgt: " + result);
+ 
+
+// Ändere die Variablen auf folgende Werte 
+
+// a = 3 
+
+a = 3;
+
+// b = a + 3 
+
+b = a + 3;
+
+// c = c -3 
+
+c = c - 3;
+
+// gib die neuen Werte der Variablen aus 
+console.log("A hat den Wert: " + a)
+console.log("B hat den Wert: " + b)
+console.log("C hat den Wert: " + c);
+
+// Rechnungsarten
+// *, /, +, -
+
+c = b - a;
+console.log("C hat den Wert: " + c)
