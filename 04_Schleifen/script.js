de1fba8563316b0ddba2323dac32ace9d98794e8
serkan <serkan.satirx61@gmail.com>
2022-11-18 09:35:00 +0100

    finished 04 Schleifen-Part2-Vid

--- 04_Schleifen/script.js
+++ 04_Schleifen/script.js
@@ -2,6 +2,6 @@ console.log("Hello");
 console.log("test");
 
 // Schleife
-for (let count = 0; count < 10; count++) {
+for (let count = 10; count > 0; count--) {
     console.log(count + " " + "Serkan");
 }
