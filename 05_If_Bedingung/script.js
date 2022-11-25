a5d4ddbd7449fb82c5d9cb195331180d364889cf
serkan <serkan.satirx61@gmail.com>
2022-11-25 08:29:52 +0100

    finished 05 If-Bedingung

--- 05_If_Bedingung/script.js
+++ 05_If_Bedingung/script.js
@@ -1,1 +1,32 @@
 console.log("Hello");
+
+let number = Math.floor(Math.random() * 100);
+
+
+// Aufgabe 1 
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini" 
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium" 
+// Wenn die Zahl größer als 50 ist gib aus "Large" 
+
+ if(number<20) {
+    console.log("Mini");
+ } else if (number <= 50) {
+    console.log("Mediums");
+ } else if(number>50) {
+    console.log("Large");
+ }
+
+ 
+
+// Aufgabe 2 
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini" 
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium" 
+// Wenn die Zahl größer als 90 ist gib aus "Large" 
+
+if(number<=7) {
+    console.log("Mini");
+ } else if (number <= 90) {
+    console.log("Medium");
+ } else if(number>90) {
+    console.log("Large");
+ }
