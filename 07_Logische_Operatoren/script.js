461b62012caa6090ecd053dd808fd7ad463a7d10
serkan <serkan.satirx61@gmail.com>
2022-11-25 08:58:29 +0100

    finished 07 Logische_Operatoren

--- 07_Logische_Operatoren/script.js
+++ 07_Logische_Operatoren/script.js
@@ -9,15 +9,19 @@ console.log("hello");
 // dann gib aus "Zahl 1 ist kleiner als Zahl 2 und Mini" 
 
  if (randomNumberOne < randomNumberTwo && randomNumberOne < 50) {
-    console.log("Zahl 1 ist kleiner als Zahl 2 und Mini");
+    console.log("Zahl 1: " + randomNumberOne + " ist kleiner als Zahl 2: " + randomNumberTwo + " und Mini");
  }
 
 // Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30 
-
 // dann gib aus "Eine der beiden ist kleiner als 30" 
 
- 
+if (randomNumberOne < 30 || randomNumberTwo < 30) {
+    console.log("Eine der beiden ist kleiner als 30: " + " Zahl1: " + randomNumberOne + " " + "Zahl2: " + randomNumberTwo);
+ }
 
 // Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist 
-
 // dann gib aus "Erste Zahl klein, zweite kein 50iger" 
+
+if (randomNumberOne < 50 && randomNumberTwo != 50) {
+    console.log("Erste Zahl kleiner als 50: " + " Zahl1: " + randomNumberOne + " zweite kein 50iger: " + " Zahl2: " + randomNumberTwo);
+ }
