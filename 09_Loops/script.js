85ecf304ccb3928f5084a9064852ea45db08198c
serkan <serkan.satirx61@gmail.com>
2022-12-13 12:46:53 +0100

    finished 09 Loops

--- 09_Loops/script.js
+++ 09_Loops/script.js
@@ -1,2 +1,20 @@
 console.log("hello");
 
+for (let row = 0; row <= 3; row++) {
+    let loopText = "";
+    for (let col = 0; col <= 3; col++) {
+        if (row % 2==0) {
+            loopText += "O";
+        } else {
+            loopText += "X";
+        }
+    }
+    console.log(loopText);
+}
+
+
+let loopSum = 0;
+for (let count = 0; count <= 99; count+=2) {
+loopSum += count;
+}
+console.log(loopSum);
