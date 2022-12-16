bcfc906943897b68548fec9281d90fe60dc37867
serkan <serkan.satirx61@gmail.com>
2022-12-16 09:30:47 +0100

    some progress on 10 Kalender

--- 10_Kalender/script.js
+++ 10_Kalender/script.js
@@ -1,1 +1,62 @@
 console.log("Hello");
+
+let month = "February"
+let startDay = 3;
+let numDays;
+
+switch (month) {
+    case "January":
+        numDays = 31;
+        break;
+    case "February":
+        numDays = 28;
+        break;
+    case "March":
+        numDays = 31;
+        break;
+    case "April":
+        numDays = 30;
+        break;
+    case "May":
+        numDays = 31;
+        break;
+    case "June":
+        numDays = 30;
+        break;
+    case "July":
+        numDays = 31;
+        break;
+    case "August":
+        numDays = 31;
+        break;
+    case "September":
+        numDays = 30;
+        break;
+    case "October":
+        numDays = 31;
+        break;
+    case "November":
+        numDays = 30;
+        break;
+    case "December":
+        numDays = 31;
+        break;
+}
+
+let calenderDays = ("| Mo | Di | Mi | Do | Fr | Sa | So |")
+console.log(calenderDays);
+
+let loopThroughDays
+if (month = "February") {
+    let result = ""
+    for (let loopThroughDays = startDay; loopThroughDays <= numDays; loopThroughDays++) {
+        result += "|  " + loopThroughDays;
+    }
+    console.log(result);
+}
+
+
+
+
+
+
