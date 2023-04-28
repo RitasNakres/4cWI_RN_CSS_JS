6ecda3ea5e220dfe0051a4aceb92edfae6c5a00c
serkan <serkan.satirx61@gmail.com>
2023-04-28 09:35:40 +0200

    changed to german bundesliga on 03_Football_API

--- JS_AND_Ajax/03_Football_API/script.js
+++ JS_AND_Ajax/03_Football_API/script.js
@@ -1,6 +1,6 @@
 const loadData = () => {
 
-    fetch("https://api.openligadb.de/getmatchdata/TSL/2008").then((result) => {
+    fetch("https://api.openligadb.de/getmatchdata/BL1/2022").then((result) => {
         result.json().then((data) => {
             fillTable(data);
         })
@@ -13,8 +13,8 @@ const fillTable = (data) => {
     data.forEach(element => {
         console.log(element)
 
-            html += `<div class="wrapper"><div class="teamOneInfo">${element.team1.teamName} ${element.matchResults[0].pointsTeam1}</div>
-            : <div class="teamTwoInfo">${element.matchResults[0].pointsTeam2} ${element.team2.teamName}</div></div>`
+            html += `<div class="wrapper"><div class="teamOneInfo" img src=${element.team1.teamIconUrl} >${element.team1.teamName} ${element.matchResults[0].pointsTeam1}</div>
+            : <div class="teamTwoInfo" img src=${element.team2.teamIconUrl}>${element.matchResults[0].pointsTeam2} ${element.team2.teamName}</div></div>`
     });
     document.getElementById("output").innerHTML = html;
 }
