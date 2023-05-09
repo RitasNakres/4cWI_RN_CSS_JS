c72e42c22c0605de2de7cb01cde0e804f835ed45
serkan <serkan.satirx61@gmail.com>
2023-05-09 12:41:48 +0200

    finished 03_Football_API

--- JS_AND_Ajax/03_Football_API/script.js
+++ JS_AND_Ajax/03_Football_API/script.js
@@ -14,9 +14,9 @@ const fillTable = (data) => {
     data.forEach(element => {
 
         if (element.matchResults.length > 0) {
-            html += `<div class="wrapper"><div class="teamOneInfo"> <img width="50px" src="${element.team1.teamIconUrl}" alt="iconTeamOne">${element.team1.shortName} ${element.matchResults[0]?.pointsTeam1}</div>
-            : <div class="teamTwoInfo"> ${element.matchResults[0]?.pointsTeam2} ${element.team2.shortName}<img width="50px" src="${element.team2.teamIconUrl}" alt="iconTeamTwo"></div></div>`
-        } else{}
+            html += `<div class="wrapper"><div class="teamOneInfo"> <div class = "teamOneIcon"><img width="66px" src="${element.team1.teamIconUrl}" alt="iconTeamOne"></div>${element.team1.shortName} ${element.matchResults[0]?.pointsTeam1}</div>
+            : <div class="teamTwoInfo"> ${element.matchResults[0]?.pointsTeam2} ${element.team2.shortName}<div class = "teamTwoIcon"><img width="50px" src="${element.team2.teamIconUrl}" alt="iconTeamTwo"></div></div></div>`
+        } else { }
 
     });
 
